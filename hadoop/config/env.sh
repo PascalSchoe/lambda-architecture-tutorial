0c026c65dace5d22861681db2b53947ac5962ad9
PascalSchoe <kamus.foryou@gmx.net>
2018-10-11 16:54:11 +0200

    trials and error #124234

--- hadoop/config/env.sh
+++ hadoop/config/env.sh
@@ -1,6 +1,6 @@
 echo "
 	export JAVA_HOME=${JAVA_HOME}
-	export HADOOP_HOME=/usr/local/hadoop
-	export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop/
+	export HADOOP_HOME=/opt/hadoop
+	export HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop/
 
-" >> /usr/local/hadoop/etc/hadoop/hadoop-env.sh
+" >> /opt/hadoop/etc/hadoop/hadoop-env.sh
--- hadoop/start-test.sh
+++ hadoop/start-test.sh
@@ -22,18 +22,18 @@ echo "Erzeuge Netzwerk fuer Hadoop"
 docker network rm hadoopnet && docker network create -d bridge --subnet 172.25.0.0/16 hadoopnet
 
 #creating image from Dockerfile in current directory
-docker build --no-cache -t pschoe/hadoop .
+docker build -t pschoe/hadoop:test .
 
 #creating container from image
 echo "starting container for master"
-docker run -itd --network="hadoopnet" --ip 172.25.0.100 -p 50070:50070 -p 8088:8088 -u hadoopuser --name master --hostname master pschoe/hadoop
+docker run -itd --network="hadoopnet" --ip 172.25.0.100 -p 50070:50070 -p 8088:8088 --name master --hostname master pschoe/hadoop:test
 
 for (( j=1; j<=$nSlaves; j++ ))
 do
 	slavename="slave$j"
 	echo "starting container for $slavename"
-	docker run -itd --network="hadoopnet" --ip 172.25.0.10$j --name $slavename --hostname $slavename pschoe/hadoop
+	docker run -itd --network="hadoopnet" --ip 172.25.0.10$j --name $slavename --hostname $slavename pschoe/hadoop:test
 done
 
-docker exec -ti master sh -c "hdfs namenode -format && /usr/local/hadoop/sbin/start-dfs.sh && /usr/local/hadoop/sbin/start-yarn.sh"
+docker exec -ti master sh -c "hdfs namenode -format && /opt/hadoop/sbin/start-dfs.sh && /opt/hadoop/sbin/start-yarn.sh"
 docker exec -ti master bash
